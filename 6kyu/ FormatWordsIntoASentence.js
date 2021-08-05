5853af9388d8a009e80225b874af455217aaf6dc
Rodrigo Ruan <rodrigopython16@gmail.com>
2021-08-05 19:38:00 -0300

    Added Format words into a sentence 6kyu

---
+++ 6kyu/ FormatWordsIntoASentence.js
@@ -0,0 +1,12 @@
+// Complete the method so that it formats the words into a single comma separated value. The last word should be separated by the word 'and' instead of a comma. The method takes in an array of strings and returns a single formatted string. Empty string values should be ignored. Empty arrays or null/nil values being passed into the method should result in an empty string being returned.
+
+// formatWords(['ninja', 'samurai', 'ronin']) // should return "ninja, samurai and ronin"
+// formatWords(['ninja', '', 'ronin']) // should return "ninja and ronin"
+// formatWords([]) // should return ""
+
+function formatWords(words) {
+  if (!words || !words.length) return ''
+  return words
+    .filter((value) => value).join(', ')
+      .replace(/(\w+), (\w+)$/g, '$1 and $2')
+}
