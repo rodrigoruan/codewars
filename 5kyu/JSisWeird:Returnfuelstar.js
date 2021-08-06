5dd8f6448e875c6fc52b765914bef4e8f34b9153
Rodrigo Ruan <rodrigopython16@gmail.com>
2021-08-06 12:32:53 -0300

    Added JS is Weird: Return fuelstar 5kyu

---
+++ 5kyu/JSisWeird:Returnfuelstar.js
@@ -0,0 +1,15 @@
+// Same as the previous JS is Weird kata, the theory is:
+
+// You can only use the following characters:
+
+// + ( ) [ ] ! x =
+
+// There are only two tests. Make the function return 'fuelstar' as a string, and don't use any banned characters.
+
+// x= is the initial solution and should be used only to define x. x is preloaded as a variable.
+
+// All other characters (including spaces) are banned.
+
+//  USED JSFUCK TO COMPLETE THIS CHALLENGE http://www.jsfuck.com/
+
+x=(![]+[])[+[]]+([][[]]+[])[+[]]+(!![]+[])[!+[]+!+[]+!+[]]+(![]+[])[!+[]+!+[]]+(![]+[])[!+[]+!+[]+!+[]]+(!![]+[])[+[]]+(![]+[])[+!+[]]+(!![]+[])[+!+[]]
