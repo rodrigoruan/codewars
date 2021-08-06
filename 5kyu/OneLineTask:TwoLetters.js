368ba6e95e3047b55acbe8d767e2647190aaa146
Rodrigo Ruan <rodrigopython16@gmail.com>
2021-08-06 12:27:45 -0300

    Added One Line Task: Two Letters 5kyu

---
+++ 5kyu/OneLineTask:TwoLetters.js
@@ -0,0 +1,3 @@
+// TASK: Convert two letters to their ASCII code(number format) and your code should in one line and less than 54 characters.
+
+toASCIINumber=(a,b)=>+(a[x="charCodeAt"]()+""+b[x]())
